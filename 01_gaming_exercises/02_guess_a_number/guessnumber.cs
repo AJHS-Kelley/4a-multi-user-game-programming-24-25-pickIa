5e895c7e51b4ad1e18afd572eafb26f1c859739b
pickIa <samsabeast101@gmail.com>
2024-09-06 14:45:14 -0400

    ya

--- 01_gaming_exercises/02_guess_a_number/guessnumber.cs
+++ 01_gaming_exercises/02_guess_a_number/guessnumber.cs
@@ -3,8 +3,30 @@ class guessNumber {
   static void Main() {
     int numGuess = 0;
     int maxGuess = 5;
-
+    int guess;
     // generate the secret number here.
+    Random rnd = new Random(); //create an object named 'rnd' that is a copy of the Random() class
+    int secretNumber = rnd.Next(10); //generate from 0 to ?
+    //int secretNumber = rnd.Next(25, 100) //generate from 25 to 999
+    while (numGuess < maxGuess)
+    {
+      Console.WriteLine("Please guess the number between 0 and 10")
+      guess = Convert.ToInt32(Console.ReadLine());
+      Console.WriteLine(guess);
+      numGuess++;
+      if (guess < secretNumber)
+      {
+        Console.WriteLine("Your guess is too low.");
+      }
+      else if (guess > secretNumber)
+      {
+        Console.WriteLine("Your guess is too high.");
+      }
+      else 
+      {
+        Console.WriteLine("Your guess is correct!")
+      }
+    }
     //while loop
       //allow user to guess the number 
       // compare the guess to the secre number
