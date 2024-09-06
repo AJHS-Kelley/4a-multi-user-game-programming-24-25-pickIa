2d24c986a359e6c5775b6591b0fa7cdb5b9ab760
pickIa <samsabeast101@gmail.com>
2024-09-06 13:57:10 -0400

    added

---
+++ 01_gaming_exercises/02_guess_a_number/guessnumber.cs
@@ -0,0 +1,16 @@
+using System;
+class guessNumber {
+  static void Main() {
+    int numGuess = 0;
+    int maxGuess = 5;
+
+    // generate the secret number here.
+    //while loop
+      //allow user to guess the number 
+      // compare the guess to the secre number
+      //if match print win, if > or <, give a hint
+      //+1 to numguess
+      //check if numguess > maxguesses
+      //if ture, print a "lose game" message, if false guess again
+  }
+}
