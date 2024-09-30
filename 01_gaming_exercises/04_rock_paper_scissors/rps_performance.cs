2220e0829cf6554507b37b5fb5b82677332b8bb3
pickIa <samsabeast101@gmail.com>
2024-09-30 13:49:16 -0400

    Create rps_performance.cs

---
+++ 01_gaming_exercises/04_rock_paper_scissors/rps_performance.cs
@@ -0,0 +1,18 @@
+using System;
+class RPS {
+  static void Main() {
+    //
+    int playerScore = 0;
+    string playerChoice = "";
+    int numDraws = 0;
+
+    //cpu variables 
+    int cpuScore = 0;
+    string cpuChoice = "";
+    int loopCount = 0;
+    int loopsReqs = 0; //req / reqs is universal abbreviations for request
+    Console.WriteLine("how many loos do you need? type and integer and press enter");
+    loopsReqs = Convert.ToInt32(Console.ReadLine);
+    while ()
+  }
+}
